68c1547845aa232a9afbf2a13d67c666f8acd6f9
flextremedev <p.wegner1992@gmail.com>
2019-09-11 21:10:21 +0200

    Cleaned up App and package.json and removed memo from button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Button.module.css';
-export const Button = React.memo(function({ children, ...props }) {
+export const Button = function({ children, ...props }) {
   return (
     <button className={styles.btn} {...props}>
       {children}
     </button>
   );
-});
+};
 Button.displayName = 'Button';
 Button.propTypes = {
   children: PropTypes.node,

afc44055e913a999099b49f12651e45e3ba74bdb
Pascal Wegner <pascal.wegner@edith.care>
2021-03-11 14:44:29 +0100

    Add timer to window title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Status } from './model/Status';
 import { FormFields } from './components/FormFields/FormFields';
 import { Counter } from './components/Counter/Counter';
 import { evaluateStatus } from './utils/evaluateStatus';
+import { format } from 'date-fns';
+import { Helmet } from 'react-helmet';
 
 function App() {
   const {
@@ -21,30 +23,38 @@ function App() {
     roundsLeft,
   } = useWorkoutTimer();
   return (
-    <div className={styles.content}>
-      <div className={styles.centerArea}>
-        {status === Status.STOPPED ? (
-          <FormFields
-            rounds={rounds}
-            handleRoundsChange={handleRoundsChange}
-            workInterval={workInterval}
-            setWorkInterval={setWorkInterval}
-            breakInterval={breakInterval}
-            setBreakInterval={setBreakInterval}
-          />
-        ) : (
-          <Counter
-            timeLeft={timeLeft}
-            text={evaluateStatus(status)}
-            roundsLeft={roundsLeft}
-            rounds={rounds}
-          />
-        )}
+    <>
+      {status !== Status.STOPPED ? (
+        <Helmet defer={false}>
+          <title>{format(timeLeft, 'mm:ss')}</title>
+        </Helmet>
+      ) : null}
+
+      <div className={styles.content}>
+        <div className={styles.centerArea}>
+          {status === Status.STOPPED ? (
+            <FormFields
+              rounds={rounds}
+              handleRoundsChange={handleRoundsChange}
+              workInterval={workInterval}
+              setWorkInterval={setWorkInterval}
+              breakInterval={breakInterval}
+              setBreakInterval={setBreakInterval}
+            />
+          ) : (
+            <Counter
+              timeLeft={timeLeft}
+              text={evaluateStatus(status)}
+              roundsLeft={roundsLeft}
+              rounds={rounds}
+            />
+          )}
+        </div>
+        <Button onClick={start} data-testid={'start-button'}>
+          {status === Status.STOPPED ? 'Start' : 'Stop'}
+        </Button>
       </div>
-      <Button onClick={start} data-testid={'start-button'}>
-        {status === Status.STOPPED ? 'Start' : 'Stop'}
-      </Button>
-    </div>
+    </>
   );
 }
 

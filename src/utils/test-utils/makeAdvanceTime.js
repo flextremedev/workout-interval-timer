a0bc96bd617ecf19d75382269d2ca0883343d638
flextremedev <p.wegner1992@gmail.com>
2021-03-27 00:51:45 +0100

    Make minutes work again

--- src/utils/test-utils/makeAdvanceTime.js
+++
@@ -1,7 +0,0 @@
-export function makeAdvanceTime(millisToAdvance, ...advanceTimeCallbacks) {
-  return function advanceTime() {
-    if (advanceTimeCallbacks.length > 0) {
-      advanceTimeCallbacks.forEach(callback => callback(millisToAdvance));
-    }
-  };
-}

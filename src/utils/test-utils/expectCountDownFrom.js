fdd5f69646b07fc9db5bdfa71257231bf2f3a5f8
flextremedev <p.wegner1992@gmail.com>
2021-03-27 01:30:21 +0100

    Refactor app test

---
+++ src/utils/test-utils/expectCountDownFrom.js
@@ -0,0 +1,40 @@
+import { act } from '@testing-library/react';
+import { ONE_SECOND_IN_MS } from '../../constants/oneSecondInMs';
+import { isLastMinute } from '../isLastMinutes';
+import { toTwoDigitString } from '../toTwoDigitString';
+
+export const expectCountDownFrom = ({
+  seconds,
+  minutes,
+  toSeconds = 1,
+  toMinutes = 0,
+  timeLeftMinutes,
+  timeLeftSeconds,
+  advanceDateNowBy,
+}) => {
+  let firstRound = true;
+
+  const shouldDisplayInitialSeconds = minutesLeft =>
+    minutesLeft >= 1 || (firstRound && minutesLeft <= 0);
+
+  for (let minutesLeft = minutes; minutesLeft >= toMinutes; minutesLeft--) {
+    let secondsToCountInMinute = shouldDisplayInitialSeconds(minutesLeft)
+      ? seconds
+      : 59;
+
+    expect(timeLeftMinutes.value).toBe(toTwoDigitString(minutesLeft));
+
+    for (
+      let secondsLeft = secondsToCountInMinute;
+      secondsLeft >= (isLastMinute(minutesLeft) ? toSeconds : 0);
+      secondsLeft--
+    ) {
+      expect(timeLeftSeconds.value).toBe(toTwoDigitString(secondsLeft));
+      advanceDateNowBy(ONE_SECOND_IN_MS);
+      act(() => {
+        jest.advanceTimersByTime(ONE_SECOND_IN_MS);
+      });
+    }
+    firstRound = false;
+  }
+};
